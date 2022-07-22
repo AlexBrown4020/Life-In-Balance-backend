ca33a9dfbd256e2b9b92fc5b59c9929957162cf8
AlexBrown4020 <balex3497@gmail.com>
2022-07-22 13:33:31 +0900

    Feat: Add classes model and migrations

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Class(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField(default=datetime.now, null=True)
+    time = models.DateTimeField(default=datetime.now, null=True)
+    description = models.TextField(max_length=255, null=True)
