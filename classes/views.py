c6e8701159c5521876efa532503644313d5a8d4a
AlexBrown4020 <balex3497@gmail.com>
2022-07-22 13:45:09 +0900

    Feat: Implement class_list view, return all classes

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .serializers import ClassSerializer
+from .models import Class
 
-# Create your views here.
+def class_list(request):
+    classes = Class.objects.all()
+    serializer = ClassSerializer(classes, many=True)
+    return JsonResponse(serializer.data)
