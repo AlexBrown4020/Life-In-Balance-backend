d0996021dc1f1473de95ee78c90e83539585cdaf
AlexBrown4020 <balex3497@gmail.com>
2022-07-25 13:14:00 +0900

    Change database and root

--- LifeInBalance/settings.py
+++ LifeInBalance/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'LifeInBalance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'ciba',
     }
 }
 
@@ -124,8 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
