5f17609e495ad8dc8804deed5f22c9eef14b0f0f
AlexBrown4020 <balex3497@gmail.com>
2022-08-04 14:50:30 +0900

    Feat: Apply password confirmation

--- members/serializers.py
+++ members/serializers.py
@@ -1,15 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
 
 class UserCreationSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=25, allow_null=False, allow_blank=False)
     email = serializers.EmailField(max_length=100, allow_null=False, allow_blank=False)
-    password = serializers.CharField(min_length=8, write_only=True)
+    password = serializers.CharField(style={'input_type': 'password'}, min_length=8, write_only=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, min_length=8, write_only=True)
 
     class Meta:
         model=User
-        fields=['username', 'email', 'password', 'image']
+        fields=['username', 'email', 'password', 'password2']
