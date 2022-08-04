880a22424e5bd111afe8ba54dcf43832bba4fe50
AlexBrown4020 <balex3497@gmail.com>
2022-08-04 15:24:24 +0900

    Fix: Remove requried, preventing server deploying

--- members/models.py
+++ members/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 class User(models.Model):
 
-    username = models.CharField(max_length=30, required=True, unique=True)
-    email = models.EmailField(required=True, unique=True)
-    password = models.CharField(required=True)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField()
