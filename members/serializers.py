28c4553c2e1a3aea44d17ccf50ae3598df8e5954
AlexBrown4020 <balex3497@gmail.com>
2022-08-05 13:44:05 +0900

    Feat: Implement initial framework for updated class model

--- members/serializers.py
+++ members/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from classes.models import Class
+
+class ClassSerializer(serializers.ModelSerializer):
+        class Meta:
+            model=Class
+            fields=['id', 'title', 'time', 'date', 'description', 'participants']
 
 class UserCreationSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=25, allow_null=False, allow_blank=False)
     email = serializers.EmailField(max_length=100, allow_null=False, allow_blank=False)
     password = serializers.CharField(style={'input_type': 'password'}, min_length=8, write_only=True)
     password2 = serializers.CharField(style={'input_type': 'password'}, min_length=8, write_only=True)
+    classes = serializers.ClassSerializer(read_only=True, many=True)
 
     class Meta:
         model=User
