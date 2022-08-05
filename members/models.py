771f4f017034bccad81f59336d61bfacf9443901
AlexBrown4020 <balex3497@gmail.com>
2022-08-05 13:38:49 +0900

    Feat: Remove instructor app, merge to User model

--- members/models.py
+++ members/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from classes.models import Class
 
 class User(models.Model):
 
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField()
+    classes = models.ForeignKey(Class, on_delete=models.CASCADE, null=True, blank=True)
+    is_instructor = models.BooleanField(default=False)
