862b9fdf63d962e0d34cbdf6b17a6ef1fcabb89f
AlexBrown4020 <balex3497@gmail.com>
2022-08-06 11:15:30 +0900

    Feat: Add create event path

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('events/', views.EventGetAllView.as_view(), name='events'),
     path('events/<int:event_id>/', views.EventView.as_view(), name='events'),
+    path('events/create/', views.CreateEventView.as_view(), name='create_event'),
 ]
