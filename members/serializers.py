7e22be0222cf9e4ab9229592f4fc2e1dfb2f71fc
AlexBrown4020 <balex3497@gmail.com>
2022-08-06 10:58:39 +0900

    Fix: Apply . to models path

--- members/serializers.py
+++ members/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Member
+from .models import Member
 from events.serializers import EventSerializer
 
 class UserCreationSerializer(serializers.ModelSerializer):
