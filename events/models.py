78db4b9aba5e8ca1fb262462eb0eef9c50b39e28
AlexBrown4020 <balex3497@gmail.com>
2022-08-08 10:11:17 +0900

    Fix: Change string of Member to object

--- events/models.py
+++ events/models.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from members.models import *
 
 class Event(models.Model):
-    creator = models.ForeignKey('Member', on_delete=models.CASCADE, related_name="creator")
+    creator = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="creator")
     title = models.CharField(max_length=200)
     date = models.DateTimeField(default=datetime.now, null=True)
     time = models.DateTimeField(default=datetime.now, null=True)
     description = models.TextField(max_length=255, null=True)
-    participants = models.ManyToManyField('Member', related_name='participants', blank=True)
+    participants = models.ManyToManyField(Member, related_name='participants', blank=True)
 
     def __str__(self):
         return self.title
