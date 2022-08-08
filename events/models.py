438755d738ddadc7b43fd3008655cbd124100479
AlexBrown4020 <balex3497@gmail.com>
2022-08-08 10:26:48 +0900

    Fix: Use FK definition

--- events/models.py
+++ events/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from datetime import datetime
-from members.models import Member
 
 class Event(models.Model):
-    creator = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="creator")
+    creator = models.ForeignKey('members.Member', on_delete=models.CASCADE, related_name="creator")
     title = models.CharField(max_length=200)
     date = models.DateTimeField(default=datetime.now, null=True)
     time = models.DateTimeField(default=datetime.now, null=True)
     description = models.TextField(max_length=255, null=True)
-    participants = models.ManyToManyField(Member, related_name='participants', blank=True)
+    participants = models.ManyToManyField('members.Member', related_name='participants', blank=True)
 
     def __str__(self):
         return self.title
